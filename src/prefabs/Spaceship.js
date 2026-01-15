ceb5a0a19d5aa00d1e0a93183d113235b3756c8d
GqbeBolt <95443729+GqbeBolt@users.noreply.github.com>
2026-01-14 22:16:58 -0800

    push again :/

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,6 +1,6 @@
 class Spaceship extends Phaser.GameObjects.Sprite {
     constructor(scene, x, y, texture, frame, pointValue) {
-        super(frame, x, y, texture, frame);
+        super(scene, x, y, texture, frame);
         scene.add.existing(this);
         this.points = pointValue;
         this.moveSpeed = 3;
@@ -12,7 +12,11 @@ class Spaceship extends Phaser.GameObjects.Sprite {
 
         // wrap from left to right edge
         if (this.x <= 0 - this.width) {
-            this.x = game.config.width;
+            this.reset();
         }
+    }
+
+    reset() {
+        this.x = game.config.width;
     }
 }
